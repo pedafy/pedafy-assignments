2b274382b3e7e268ff7946d3d5a5a01f65e26b6f
Florent Poinsard <florent.poinsard@epitech.eu>
2019-04-30 12:41:37 +0200

    Modification of the dev environment

---
+++ tig.go
@@ -0,0 +1,16 @@
+package pedafyassignments
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world! Are we running in production: %v\n", !appengine.IsDevAppServer())
+}
